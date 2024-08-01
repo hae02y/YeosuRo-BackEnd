335978c3600372db4af7086c44a59178478981ce
hae02y <godud1118@gmail.com>
2024-08-01 23:07:38 +0900

    feat: SiteController

--- src/main/java/greenjangtanji/yeosuro/site/controller/SiteController.java
+++ src/main/java/greenjangtanji/yeosuro/site/controller/SiteController.java
@@ -1,11 +1,20 @@
 package greenjangtanji.yeosuro.site.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("/v1/site")
+@RequestMapping("/v1/sites")
 public class SiteController {
 
+    @GetMapping("")
+    public ResponseEntity<> getMySites() {
+
+        return (ResponseEntity) ResponseEntity.ok();
+    }
 
 }
